d457154bc723ed3bc918179a8c35d7171932af65
sharath <2004sharath@gmail.com>
2025-12-08 14:27:40 +0530

    update

---
+++ .next/dev/static/chunks/app_school-registration_page_tsx_cb70b12a._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_1779ae64._.js",
+  "static/chunks/node_modules__pnpm_7ef5f3a1._.js"
+],
+    source: "dynamic"
+});
